f2a22e96090e39f08fcea2408c5ba5824d3f3761
Surdeanu Mihai <mihaisurdeanu.ro>
2014-04-06 14:09:28 +0300

    Un alt test

--- Septica/src/ro/pirvulescusurdeanu/septica/MainActivity.java
+++ Septica/src/ro/pirvulescusurdeanu/septica/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends Activity {
     	// Simplu test
         public PlaceholderFragment() {
         }
+        // Test
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
